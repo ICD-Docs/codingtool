d893be22d73ef5696131a586c9a53436a093f04b
Marc Donada <marc.donada@outlook.com>
2024-01-25 15:28:36 +0100

    merged all languages docker scripts in one script

--- local-live-edit.ps1
+++ local-live-edit.ps1
@@ -1,10 +1,15 @@
+${lang} = "en" # select a language: ar cs de en es fr pl pt ru sk tr zh uz 
+
+Write-Output "current language: ${lang}"
+
 # RUN THE docs for live editing. Run to create the container
 docker run -ti `
-           --name codingtool-fr-mkdocs `
+           --name codingtool-${lang}-mkdocs `
            -p 8080:8000 `
-           -v C:/code/GitRepos/Docs/codingtool/fr:/mkdocs `
+           -v C:/code/GitRepos/Docs/codingtool/${lang}:/mkdocs `
            -v C:/code/gitrepos/DocsBuilder/docbuilder/custom_readthedocs:/mkdocs/custom_readthedocs `
            --env LIVE_RELOAD_SUPPORT=true `
            polinux/mkdocs 
+
 # If it is already created, make sure it's running
-docker start codingtool-fr-mkdocs
+docker start codingtool-${lang}-mkdocs
